f516c12ea33a50c49f998d262c1ee3ae6306b162
l-pedretti <pedroleon99@hotmail.it>
2021-05-22 18:09:38 +0200

    Added serach parameters retrieval (not finished)

--- server-express.js
+++ server-express.js
@@ -54,11 +54,26 @@ server.post('/login', function(request, response) {
 	}
 });
 
+function searchParams() {
+		dbconnection.query('SELECT DISTINCT categoria FROM proprieta; SELECT DISTINCT tipo FROM proprieta; SELECT DISTINCT citta FROM proprieta', function(error, results) { 		
+			if (error) throw error;
+			params = [];
+			for(var i=0; i < results[0].length; i++) {
+				params.push(results[0][i]);
+			}
+			for(var i=0; i < results[1].length; i++) {
+				params.push(results[1][i]);
+			}
+			console.log(params);
+			proprieta = "ciao";
+			return proprieta;
+		});
+}
+
+server.locals.searchParams = searchParams();
+
 server.get('/home', function(request, response) {
 	if (request.session.loggedin) {
-		dbconnection.query('SELECT DISTINCT categoria FROM proprieta; SELECT DISTINCT tipo FROM proprieta; SELECT DISTINCT citta FROM proprieta', function(error, results, fields) { 
-		//
-		});
 		response.render('index');
 	} else {
 		response.send('Please login to view this page!');
@@ -99,8 +114,6 @@ server.get('/agenti/:id', function(request, response) {
 
 server.get('/proprieta', function(request, response) {
 	if (request.session.loggedin) {
-		dbconnection.query('SELECT DISTINCT categoria FROM proprieta; SELECT DISTINCT tipo FROM proprieta; SELECT DISTINCT citta FROM proprieta', function(error, results, fields) { 
-		});
 		dbconnection.query('SELECT * FROM proprieta', function(error, results, fields) {
 			if (error) throw error;
 			if (results) {
@@ -119,9 +132,6 @@ server.get('/proprieta', function(request, response) {
 
 server.get('/proprieta/:id', function(request, response) {
 	if (request.session.loggedin) {
-		dbconnection.query('SELECT DISTINCT categoria FROM proprieta; SELECT DISTINCT tipo FROM proprieta; SELECT DISTINCT citta FROM proprieta', function(error, results, fields) { 
-		//
-		});
 		dbconnection.query('SELECT * FROM proprieta WHERE id = ?', [request.params.id], function(error, results, fields) {
 			if (results) {
 				var proprieta = JSON.stringify(results);
@@ -136,9 +146,6 @@ server.get('/proprieta/:id', function(request, response) {
 
 server.get('/proprieta/:categoria/:contratto', function(request, response) {
 	if (request.session.loggedin) {
-		dbconnection.query('SELECT DISTINCT categoria FROM proprieta; SELECT DISTINCT tipo FROM proprieta; SELECT DISTINCT citta FROM proprieta', function(error, results, fields) { 
-		//
-		});
 		if(request.params.contratto == 'affitto'){
 			dbconnection.query('SELECT * FROM proprieta WHERE categoria = ?  AND affitto = 1', [request.params.categoria], function(error, results, fields) {
 				if (error) throw error;
